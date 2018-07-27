6b20e220cd3c7b5273dc4623970fc81bae9207b6
Scotty F <scottymferreira@gmail.com>
2018-07-27 17:19:23 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = {
+  PearlJam: 'Black',
+  Soundgarden: 'Spoon Man',
+  Rush: '2112',
+  Metallica: 'One'
+};
+
+function updatePlaylist(list, artistName, songTitle){
+  list [artistName] = songTitle
+  return list
+}
+updatePlaylist(playlist, 'Pink Floyd', 'The Wall' )
+
+function removeFromPlaylist(list, artistName){
+  delete list.artistName;
+}
+removeFromPlaylist(playlist, Soundgarden)
